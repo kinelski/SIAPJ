583be3714a69d5c5dbe3406e45363f2c2674304b
Gabriel Brito <gabriel.brito55@gmail.com>
2017-04-13 22:24:56 -0300

    Refatoração realizada no validador de processos

--- src/ControladorSIAPJ.java
+++ src/ControladorSIAPJ.java
@@ -1,5 +1,7 @@
 
-public class ControladorSIAPJ implements ValidadorProcesso{
+public class ControladorSIAPJ{
+	private ValidadorProcesso validator = new ValidadorProcesso();
+	
 	
 	public boolean initProcesso(Processo proc){
 		if(!checkProcesso(proc))
@@ -8,7 +10,7 @@ public class ControladorSIAPJ implements ValidadorProcesso{
 	}
 	
 	private boolean checkProcesso(Processo proc){
-		return validateProcess(proc);
+		return validator.validateProcess(proc);
 	}
 	
 	private Processo persistProcesso(Processo proc){
@@ -17,25 +19,5 @@ public class ControladorSIAPJ implements ValidadorProcesso{
 	
 	private void sendInfoByEmail(Processo proc, boolean statusProcesso){
 		
-	}
-	
-	public boolean validateProcess(Processo proc){
-		String id = Integer.toString(proc.getId());
-		String nome = proc.getNomeReclamante();
-		String tel = proc.getTelefone();
-		String email = proc.getEmail();
-		//id deve ser uma string contendo apenas numeros
-		if(!id.matches("[0-9]+"))
-			return false;
-		//nome deve conter apenas letras e espaços
-		if(!nome.matches("[a-zA-Z ]+"))
-			return false;
-		//Telefone deve ser da forma (xx)xxxxx-xxxx
-		if(!tel.matches("[(][0-9]{2}[)][0-9]{5}-[0-9]{4}"))
-			return false;
-		if(!email.matches("[a-z0-9-_]+@[a-z0-9]+([.][a-z]+)+"))
-			return false;
-		//Assume-se que a reclamação pode ser uma string qualquer
-		return true;
 	}
 }
